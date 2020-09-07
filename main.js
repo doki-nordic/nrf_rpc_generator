b63736233a6903f87f88df272ce55226430b6e50
Dominik Kilian <Dominik.Kilian@nordicsemi.no>
2020-09-07 11:02:20 +0200

    Temp files cleanup and new param/fields kinds

--- main.js
+++ main.js
@@ -2,7 +2,24 @@ require('./lib/Polyfill').exec();
 
 const Options = require('./lib/Options');
 const Units = require('./lib/Units');
+const { pathType } = require('./lib/Utils');
 const fs = require('fs');
+const path = require('path');
+
+function rmRecursive(name)
+{
+	let type = pathType(name);
+	if (type == 'file') {
+		fs.unlinkSync(name);
+	} else if (type == 'dir') {
+		for (let f of fs.readdirSync(name)) {
+			if (f == '.' || f == '..') continue;
+			rmRecursive(path.join(name, f));
+		}
+		fs.rmdirSync(name);
+	}
+}
+
 
 try {
 	Options.parseParams();
@@ -12,6 +29,10 @@ try {
 	mod.save();
 } finally {
 	if (Options.options.tmpDir !== null) {
-		//TODO: fs.rmdirSync(Options.options.tmpDir, { recursive: true });
+		if (Options.options.dumpIntermediateFiles) {
+			console.log(`Skipping removal of temporary directory: ${Options.options.tmpDir}`);
+		} else {
+			rmRecursive(Options.options.tmpDir);
+		}
 	}
 }
